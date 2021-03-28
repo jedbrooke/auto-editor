8a7ff514f4269f0f0fe8d1a88167ef93179598ee
jsdavids <67972461+jsdavids@users.noreply.github.com>
2021-03-28 03:16:51 -0700

    improved drag and drop ui

--- electron/filedrag.js
+++ electron/filedrag.js
@@ -43,6 +43,12 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 
 	}
 
+	function TextUnderline(e){
+		e.stopPropagation();
+		e.preventDefault();
+		e.target.className = (e.type == "messages" ? "hover" : "");
+	}
+
 
 	// output file information
 	function ParseFile(file) {
@@ -65,7 +71,8 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 	function Init() {
 
 		var fileselect = $id("filePath"),
-			filedrag = $id("filedrag");
+			filedrag = $id("filedrag"),
+			m = $id("messages");
 		$id("exportButton").disabled = true;
 		// file select
 		fileselect.addEventListener("change", FileSelectHandler, false);
@@ -78,6 +85,7 @@ Developed by Craig Buckler (@craigbuckler) of OptimalWorks.net
 			filedrag.addEventListener("dragover", FileDragHover, false);
 			filedrag.addEventListener("dragleave", FileDragHover, false);
 			filedrag.addEventListener("drop", FileSelectHandler, false);
+			m.addEventListener("mouseover", TextUnderline, false);
 			filedrag.style.display = "block";
 		}
 
