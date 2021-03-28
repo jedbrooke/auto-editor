c55b7579115033b2d29d369014a52107b4b52e36
Jasper Edbrooke <jasperredrock@gmail.com>
2021-03-28 07:57:55 -0700

    change export path in process.php

--- electron/process.php
+++ electron/process.php
@@ -3,6 +3,6 @@
     // run command
     // when command is done
     shell_exec("$argv[1]");
-    rename("/home/ubuntu/exports/$argv[2].mp4","/var/www/html/downloads/$argv[2].mp4");
+    rename("/var/www/exports/$argv[2].mp4","/var/www/html/downloads/$argv[2].mp4");
     // unlink("/home/ubuntu/imports/$argv[2].mp4");
 ?>
